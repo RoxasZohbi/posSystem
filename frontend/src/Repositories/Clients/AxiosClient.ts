37b8e4876dc609e4f23a5f7381665d16ccea2a02
Syed Zohaib Hussain <roxas.zohbi786@gmail.com>
2026-06-27 20:22:52 +0500

    feat: use VITE_API_URL env variable for axios baseURL

--- frontend/src/Repositories/Clients/AxiosClient.ts
+++ frontend/src/Repositories/Clients/AxiosClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const Client = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.VITE_API_URL as string,
   headers: { 'Content-Type': 'application/json', Accept: 'application/json' },
   withCredentials: true,
 })
